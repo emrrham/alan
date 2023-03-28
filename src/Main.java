f645ff99d032bf527388d4c68df8db10c0b999b9
emrrham <mremir255@gmail.com>
2023-03-28 18:29:39 +0300

    proje

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class alanhesap {
+    public static void alanhesap(String[] args){
+        int a , b;
+        double c;
+        Scanner girdi = new Scanner(System.in);
+        System.out.println("Birinci kenarı giriniz");
+        a = girdi.nextInt()
+        System.out.println("İkinci kenarı giriniz");
+        b = girdi.nextInt();
+        c = Math.sqrt((a*a)+(b*b))
+        System.out.println("Hipotenüs kenarı" + c);
+
+    }
+
+
+}
